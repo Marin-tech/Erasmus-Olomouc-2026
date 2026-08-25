eb6f1097ded7288f8a84a9944d0df7cb15eaef7c
Marin-tech <r.marini.work@gmail.com>
2026-08-25 13:27:37 +0200

    Refresh the offline app shell

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='olomouc-quest-v5';
+const CACHE='olomouc-quest-v6';
 const SHELL=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
